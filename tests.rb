4f424801b821b621a7f253b2653d8f9d88d61073
Ashley Mason <amason@spoonflower.com>
2015-09-18 12:18:29 -0400

    Validation tests for users first and last name tested/passing.

--- tests.rb
+++ tests.rb
@@ -28,33 +28,57 @@ class ApplicationTest < Minitest::Test
     assert true
   end
 
+#validation in school
   def test_schools_must_have_names
     assert School.create(name: "Grover C. Fields")
     s = School.new()
     refute s.save
   end
 
+#validation in assignment
   def test_assignments_have_a_course_id
     a = Assignment.new()
     refute a.save
   end
 
+#validation in assignment
   def test_assigments_have_names
     assert Assignment.create(name: "Homework")
     b = Assignment.new()
     refute b.save
   end
 
+#validation in assignment
   def test_assignments_have_percent_of_grade
     assert Assignment.create(percent_of_grade: 30)
     c = Assignment.new()
     refute c.save
   end
 
+#validation in assignment
   def test_no_duplicate_assignments
     assert Assignment.create(name: "Friday Homework")
     e = Assignment.new(name: "Friday Homework")
     refute e.save
+  end
+
+#validation in user
+  def test_no_duplicate_emails
+    assert User.create(email: "fake@gmail.com")
+    f = User.new(email: "fake@gmail.com")
+    refute f.save
+  end
+
+  def test_user_has_first_name
+    assert User.create(first_name: "Andy")
+    g = User.new()
+    refute g.save
+  end
+
+  def test_user_has_last_name
+    assert User.create(last_name: "Warhol")
+    h = User.new()
+    refute h.save
   end
 
 end
