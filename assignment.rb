e5a5fedde627a5fca1c95089fa13e8e8ea22b7e2
Anthony Ellis <ajellis1989@gmail.com>
2015-09-20 02:27:09 -0400

    Adds course dependency for assignments

--- assignment.rb
+++ assignment.rb
@@ -1,6 +1,6 @@
 class Assignment < ActiveRecord::Base
 
-  belongs_to :courses
+  belongs_to :courses, dependent: :destroy
 
   scope :active_for_students, -> { where("active_at <= ? AND due_at >= ? AND students_can_submit = ?", Time.now, Time.now, true) }
 
