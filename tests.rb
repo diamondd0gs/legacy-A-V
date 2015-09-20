c2a069cb938e261074c2469ab8a66ee57f01c4ff
Anthony Ellis <ajellis1989@gmail.com>
2015-09-20 02:17:07 -0400

    Fixed course students association with courses

--- tests.rb
+++ tests.rb
@@ -30,86 +30,91 @@ class ApplicationTest < Minitest::Test
     assert true
   end
 
-  def test_lessons_must_have_names
-    a = Lesson.new(name: "Mathematics")
-    s = School.new()
-    assert a.save
-    refute s.save
-  end
-
-  def test_readings_must_have_number_order
-    r = Reading.new(url: "cooladdress.com", lesson_id: 45)
-    refute r.save
-  end
-
-  def test_readings_must_have_lesson_id
-    r = Reading.new(order_number: 1, url: "cooladdress.com")
-    refute r.save
-  end
-
-  def test_readings_must_have_url
-    r = Reading.new(order_number: 1, lesson_id: 45)
-    refute r.save
-  end
-
-  def test_courses_must_have_course_code
-    a = Course.create(name: "Mathematics", course_code: "mat101")
-    c = Course.new(name: "Mathematics")
-    assert a.save
-    refute c.save
-  end
-
-  def test_courses_must_have_name
-    a = Course.create(name: "Mathematics", course_code: "mat101")
-    c = Course.new(course_code: "math")
-    assert a.save
-    refute c.save
-  end
-
-  def test_courses_must_have_unique_course_code_per_term
-    a = Course.create(name: "Mathematics", course_code: "mat101")
-    c = Term.create(name: "fall")
-    a.term_id = c
-    assert a.save
-
-   b = Course.new(name: "Mathematics", course_code: "mat101")
-   b.term_id = c
-   refute b.save
-  end
-
-  def test_course_code_starts_3_letters_ends_3_numbers
-    a = Course.new(name: "Intro to Mathematics", course_code: "MAT101")
-    b = Course.new(name: "another course", course_code: "mat101")
-    c = Course.new(name: "yet another course", course_code: "MATH101")
-
-    assert a.save
-    assert b.save
-    refute c.save
-  end
-
-
-  def test_schools_and_terms_are_associated_correctly
-    a = School.create(name: "Anthony High School")
-    c = Term.create(name: "fall")
-    a.terms << c
-    assert a.reload.terms.include?(c)
-    assert_equal a, c.reload.school
-  end
-
+  # def test_lessons_must_have_names
+  #   a = Lesson.new(name: "Mathematics")
+  #   s = School.new()
+  #   assert a.save
+  #   refute s.save
+  # end
+  #
+  # def test_readings_must_have_number_order
+  #   r = Reading.new(url: "cooladdress.com", lesson_id: 45)
+  #   refute r.save
+  # end
+  #
+  # def test_readings_must_have_lesson_id
+  #   r = Reading.new(order_number: 1, url: "cooladdress.com")
+  #   refute r.save
+  # end
+  #
+  # def test_readings_must_have_url
+  #   r = Reading.new(order_number: 1, lesson_id: 45)
+  #   refute r.save
+  # end
+  #
+  # def test_courses_must_have_course_code
+  #   a = Course.create(name: "Mathematics", course_code: "mat101")
+  #   c = Course.new(name: "Mathematics")
+  #   assert a.save
+  #   refute c.save
+  # end
+  #
+  # def test_courses_must_have_name
+  #   a = Course.create(name: "Mathematics", course_code: "mat101")
+  #   c = Course.new(course_code: "math")
+  #   assert a.save
+  #   refute c.save
+  # end
+  #
+  # def test_courses_must_have_unique_course_code_per_term
+  #   a = Course.create(name: "Mathematics", course_code: "mat101")
+  #   c = Term.create(name: "fall")
+  #   a.term_id = c
+  #   assert a.save
+  #
+  #  b = Course.new(name: "Mathematics", course_code: "mat101")
+  #  b.term_id = c
+  #  refute b.save
+  # end
+  #
+  # def test_course_code_starts_3_letters_ends_3_numbers
+  #   a = Course.new(name: "Intro to Mathematics", course_code: "MAT101")
+  #   b = Course.new(name: "another course", course_code: "mat101")
+  #   c = Course.new(name: "yet another course", course_code: "MATH101")
+  #
+  #   assert a.save
+  #   assert b.save
+  #   refute c.save
+  # end
+  #
+  #
+  # def test_schools_and_terms_are_associated_correctly
+  #   a = School.create(name: "Anthony High School")
+  #   c = Term.create(name: "fall")
+  #   a.terms << c
+  #   assert a.reload.terms.include?(c)
+  #   assert_equal a, c.reload.school
+  # end
+  #
   def test_courses_and_terms_are_associated_correctly
-    a = Course.create(name: "Intro to Mathematics", course_code: "MAT101")
-    c = Term.create(name: "fall")
-    assert a.term_id = c.id
+    a = Term.create(name: "fall")
+    c = Course.create(name: "Intro to Mathematics", course_code: "MAT101")
+    a.courses << c
+    assert a.reload.courses.include?(c)
   end
 
   def test_courses_and_course_students_are_associated_correctly
     a = Course.create(name: "Intro to Mathematics", course_code: "MAT101")
     c = CourseStudent.create(student_id: "1")
-    assert a.save
-    assert c.save
-    assert c.course_id = a.id
+    a.course_students << c
+    assert a.reload.course_students.include?(c)
   end
 
+# def test_courses_and_assignments_are_associated_correctly
+#   a = Course.create(name: "Intro to Mathematics", course_code: "MAT101")
+#   c = Assignment.new(name: "Battleship (MUAHAHAHAHA!)")
+#   assert a.save
+#   assert c.save
 
 
 
